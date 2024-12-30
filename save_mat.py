5f9ca6aeec505d59664779fb12f51d4f45ca1b4d
oldbiy <121919449@qq.com>
2024-12-30 23:01:41 +0800

    Update save_mat.py

--- save_mat.py
+++ save_mat.py
@@ -14,15 +14,12 @@ d_path = []
 ir_path = []
 vis_path = []
 for i in range(30):
-    d_name = os.path.join(ori_path, 'D%d.png' % (i+1))
     ir_name = os.path.join(ori_path, 'IR%d.png' % (i+1))
     vis_name = os.path.join(ori_path, 'VIS%d.png' % (i+1))
-    d = cv2.imread(d_name, 0)
     ir = cv2.imread(ir_name, 0)
     vis = cv2.imread(vis_name, 0)
-    h, w = d.shape
-    cube = np.zeros((3, h, w))
+    h, w = ir.shape
+    cube = np.zeros((2, h, w))
     cube[0, :, :] = ir
     cube[1, :, :] = vis
-    cube[2, :, :] = d
     io.savemat(out_path + '%d.mat' % (i+1), {'data':cube})
